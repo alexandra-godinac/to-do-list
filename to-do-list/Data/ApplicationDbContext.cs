04606b8a878405096461ec4eb25d6db93731d34c
alexandra-godinac <alexandragodinac@yahoo.com>
2024-03-16 18:56:41 +0200

    project foundation

---
+++ to-do-list/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace to_do_list.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
